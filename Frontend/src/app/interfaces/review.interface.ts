d28c8e249c5019cc55444568301920eedec11b69
Ale Verdugo <verdugolineroalejandro@gmail.com>
2025-06-14 10:14:30 +0200

    Mejoras en diseño

--- Frontend/src/app/interfaces/review.interface.ts
+++ Frontend/src/app/interfaces/review.interface.ts
@@ -3,6 +3,7 @@ import { User } from "./user.interface";
 export interface Review {
     id: number;
     user: User;
+    user_id: number;
     score: number;
     comment: string;
     date: string;
